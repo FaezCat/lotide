cc09c524438d553b47e328d9bcdeddc906b2a69a
faez <faezc@hotmail.com>
2021-09-15 21:37:03 +0000

    completed countLetters.js file

---
+++ countLetters.js
@@ -0,0 +1,14 @@
+const countLetters = function(sentence) {
+  const results = {};
+
+  for (const letter of sentence) {
+    if (letter !== " ") {
+      if (results[letter]) {
+        results[letter] += 1;
+      } else {
+        results[letter] = 1;
+      }
+    }
+  }
+  return results;
+};
