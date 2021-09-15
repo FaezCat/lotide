db0be73d6123b62a131640863389251b7442f707
faez <faezc@hotmail.com>
2021-09-15 22:13:08 +0000

    corrected the countOnly and letterPositions files after accidentally saving over the logic of one with the other

---
+++ letterPositions.js
@@ -0,0 +1,17 @@
+const countLetters = function(sentence) {
+  const results = {};
+
+  for (let i = 0; i < sentence.length; i++) {
+    if (sentence[i] !== " ") {
+      if (results[sentence[i]]) {
+        results[sentence[i]].push(i);
+      } else {
+        results[sentence[i]] = [i];
+      }
+    }
+  }
+
+  return results;
+};
+
+console.log(countLetters("lighthouse in the house"));
