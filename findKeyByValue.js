e68e593a77d67e9ace36bcffe7b0f9b85a3abc65
faez <faezc@hotmail.com>
2021-09-16 01:53:08 +0000

    adjusted findKeyByValue file with two possible solutions

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,9 +1,18 @@
+// const findKeyByValue = function(object, value) {
+//   const valuesList = Object.values(object);
+//   const keysList = Object.keys(object);
+//   for (let i = 0; i < valuesList.length; i++) {
+//     if (valuesList[i] === value) {
+//       return keysList[i];
+//     }
+//   }
+// };
+
 const findKeyByValue = function(object, value) {
-  const valuesList = Object.values(object);
   const keysList = Object.keys(object);
-  for (let i = 0; i < valuesList.length; i++) {
-    if (valuesList[i] === value) {
-      return keysList[i];
+  for (const key of keysList) {
+    if (object[key] === value) {
+      return key;
     }
   }
 };
