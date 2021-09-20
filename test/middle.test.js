96cf7a3d99506556910c13a65becf2fa8495bc6f
faez <faezc@hotmail.com>
2021-09-20 22:15:20 +0000

    refactored middle.test and tail.test files to make use of chai

--- test/middle.test.js
+++ test/middle.test.js
@@ -1,4 +1,14 @@
 const middle = require("../middle");
-const assertArraysEqual = require("../assertArraysEqual");
+const assert = require("chai").assert;
 
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+describe("#middle", () => {
+  it('should return the middle value [3]', () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+});
+
+describe("#middle", () => {
+  it('should return the middle values [2, 3]', () => {
+    assert.deepEqual(middle([0, 1, 2, 3, 4, 5]), [2, 3]);
+  });
+});
--- test/tail.test.js
+++ test/tail.test.js
@@ -1,5 +1,14 @@
-const assertArraysEqual = require("../assertArraysEqual");
+const assert = require("chai").assert;
 const tail = require("../tail");
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertArraysEqual(result, ["Lighthouse", "Labs"]);
+describe("#tail", () => {
+  it("should return ['Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
+});
+
+describe("#tail", () => {
+  it("should return [1, 2]", () => {
+    assert.deepEqual(tail([0, 1, 2]), [1, 2]);
+  });
+});
